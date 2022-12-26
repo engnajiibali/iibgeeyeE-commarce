3a79c2b1f756321a1044535fe17a78b13aef63c6
engnajiibali <117753826+engnajiibali@users.noreply.github.com>
2022-12-26 18:50:30 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Backend\AdminProfileController;
+use App\Http\Controllers\Backend\BrandController;
+use App\Http\Controllers\FrontEnd\IndexController;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +17,7 @@ use App\Http\Controllers\Backend\AdminProfileController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::middleware('admin:admin')->group(function () {
     Route::get('admin/login', [AdminController::class, 'loginForm']);
@@ -36,15 +37,40 @@ Route::middleware(['auth:sanctum,admin', config('jetstream.auth_session'), 'veri
 Route::get('admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
 Route::get('admin/profile', [AdminProfileController::class, 'AdminProfile'])->name('admin.profile');
 Route::get('admin/Edit/Profile', [AdminProfileController::class, 'Admin_Edit_Profile'])->name('admin.profile.Edit');
-  Route::post('admin/profile/Store', [AdminProfileController::class, 'admin_profile_store'])->name('admin.profile.store');
+Route::post('admin/profile/Store', [AdminProfileController::class, 'admin_profile_store'])->name('admin.profile.store');
+Route::get('admin/Change/Password', [AdminProfileController::class, 'admin_change_password'])->name('admin.change.password');
+Route::post('admin/Update/Password', [AdminProfileController::class, 'admin_Update_Password'])->name('admin.update.password');
+
 
  
+
+
+#user controoler
+Route::get('/', [IndexController::class, 'index']);
+Route::get('User/logout', [IndexController::class, 'userlogout'])->name('user.logout');
+Route::get('User/profile', [IndexController::class, 'userprofile'])->name('user.profile');
+Route::post('user/profile/Store', [IndexController::class, 'usern_profile_store'])->name('user.profile.store');
+Route::get('User/change/passwor', [IndexController::class, 'user_change_passwor'])->name('user.change.passwor');
+Route::post('user/Update/Password', [IndexController::class, 'user_Update_Password'])->name('user.profile.updatePassword');
+
+#Barands
+Route::prefix('Brand')->group(function(){
+Route::get('/View', [BrandController::class, 'BrandView'])->name('all.Barand');    
+
+});
+
+
+
+
+
 
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+         $id =Auth::user()->id;
+        $user =User::find($id);
+        return view('dashboard',compact('user'));
     })->name('dashboard');
 });
  
